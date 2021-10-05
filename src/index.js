3b6b31f1d7a451b169cb04aef94706aa8bc99639
TiavinaMichael <ralainirinatiavina@gmail.com>
2021-10-05 14:55:31 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,42 @@
+import React, { useState, useCallback } from 'react'
+import ReactDOM from 'react-dom'
+import Slider from '@material-ui/core/Slider'
+import Cropper from 'react-easy-crop'
+import './styles.css'
+
+const App = () => {
+  const [crop, setCrop] = useState({ x: 0, y: 0 })
+  const [zoom, setZoom] = useState(1)
+  const onCropComplete = useCallback((croppedArea, croppedAreaPixels) => {
+    console.log(croppedArea, croppedAreaPixels)
+  }, [])
+  return (
+    <div className="App">
+      <div className="crop-container">
+        <Cropper
+          image="https://img.huffingtonpost.com/asset/5ab4d4ac2000007d06eb2c56.jpeg?cache=sih0jwle4e&ops=1910_1000"
+          crop={crop}
+          zoom={zoom}
+          aspect={4 / 3}
+          onCropChange={setCrop}
+          onCropComplete={onCropComplete}
+          onZoomChange={setZoom}
+        />
+      </div>
+      <div className="controls">
+        <Slider
+          value={zoom}
+          min={0.5}
+          max={3}
+          step={0.1}
+          aria-labelledby="Zoom"
+          onChange={(e, zoom) => setZoom(zoom)}
+          classes={{ root: 'slider' }}
+        />
+      </div>
+    </div>
+  )
+}
+
+const rootElement = document.getElementById('root')
+ReactDOM.render(<App />, rootElement)
